a89f1f96f1c052f1f80f9b9f691711b6b72d57c6
Pouria-GH <pouria.ghafarzadeh@gmail.com>
2025-09-01 08:52:42 +0330

    initial database

---
+++ src/CleanCrud.Presistence/DbContexts/ApplicationDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCrud.Presistence.DbContexts
+{
+    internal class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            options.UseSqlServer("Server=CHICKEN-WINGS\\NEWSQLSERVER;Database=CleanCrud;User Id=CleanLogin;Password=Clean.123;TrustServerCertificate=true;");
+
+            return new ApplicationDbContext(options.Options);
+        }
+    }
+}
