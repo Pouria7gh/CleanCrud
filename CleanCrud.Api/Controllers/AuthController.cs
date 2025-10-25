d69e5fb8e7198ff2a28dedcaf884c3216a64b3e5
Pouria-GH <pouria.ghafarzadeh@gmail.com>
2025-10-25 16:05:59 +0330

    feat(api): add auth controller.

---
+++ CleanCrud.Api/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanCrud.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("Register")]
+        public IActionResult Register()
+        {
+            return Ok();
+        }
+    }
+}
