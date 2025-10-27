7a0e2c0df302616cb65867d03153acdaef1447ae
Pouria-GH <pouria.ghafarzadeh@gmail.com>
2025-10-27 16:36:00 +0330

    feat(api): add dbcontext  and identity to services in program cs.

--- CleanCrud.Api/Program.cs
+++ CleanCrud.Api/Program.cs
@@ -1,3 +1,8 @@
+using CleanCrud.Domain.Entities;
+using CleanCrud.Presistence.DbContexts;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,11 +15,23 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
+        // fix these later
         policy.AllowAnyOrigin()
               .AllowAnyMethod()
               .AllowAnyHeader();
     });
 });
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")) );
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.Password.RequiredLength = 6;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultTokenProviders();
 
 var app = builder.Build();
 
